881731182f87d077fbb7169f9d10fab6d112fda0
arnav-s1ngh <92050142+arnav-s1ngh@users.noreply.github.com>
2025-01-17 14:53:13 +0530

    Create code.py

---
+++ Programming Exercise-1/code.py
@@ -0,0 +1,63 @@
+#Transposition Cipher
+import string
+from time import sleep
+import math
+universal_set=list(string.ascii_lowercase)
+# Hashing
+# Reference:- https://gist.github.com/amakukha/7854a3e910cb5866b53bf4b2af1af968
+# Initially used djb2, but as pointed out on this webpage ☝️, it suffers from avalanching
+def hash_sdbm(s):
+    hash=0
+    for x in s:
+        hash=((hash << 16)+(hash << 6)+ord(x)-hash) & 0xFFFFFFFF
+    return "".join([universal_set[int(i)] for i in str(hash)])
+print(hash_sdbm("whatislovebabydonthurtmedonthurtmenomore"))
+
+
+#Encryption
+
+print("Encryption Initiated....\n")
+plaintext=input("Original Text:- ")
+if not set([i for i in plaintext]).issubset(set(universal_set)):
+    print("Illegal Input. Exiting Safely.")
+    exit()
+sleep(1)
+print("Plaintext is acceptable\n")
+key_size=int(input("Key Size [less than or equal to 9]:- "))
+if not (key_size>=0 and key_size<=9):
+    print("Illegal Input. Exiting Safely.")
+    exit()
+sleep(1)
+print("Key Size is acceptable\n")
+key=input("Enter the key without any spaces:- ")
+if len(key)!=key_size or not set([i for i in key]).issubset(set(["1","2","3","4","5","6","7","8","9"])):
+    print("Illegal Input. Exiting Safely.")
+    exit()
+sleep(1)
+print("Key is acceptable\n")
+plaintext+=hash_sdbm(plaintext)
+# print(plaintext)
+# Number of Columns in the table == key_size
+# Number of Rows in the table == ceil(len(concatenatedtext)/key_size)
+# print("Padding Size:- ", (key_size*math.ceil(len(plaintext)/key_size)-len(plaintext)))
+table=[['q']*key_size for i in range(math.ceil(len(plaintext)/key_size))]
+for i in range(len(plaintext)):
+    table[i//key_size][i%key_size]=plaintext[i]
+key=[int(i) for i in key]
+indices=list(range(len(table[0])))
+indices.sort(key=lambda x:key[x])
+cipher_text=""
+for i in indices:
+    for rownum in range(len(table)):
+        cipher_text+=str(table[rownum][i])
+print("Sent Cipher:- ", cipher_text,"\n")
+print("The plaintext has been successfully encrypted\n")
+
+#Decryption
+print("Decryption Initiated")
+received_text=input("Cipher Text:- ")
+received_key=int(input("Enter the key without any spaces:- "))
+
+
+
+#Brute Force Attack
