bca0e1f75ac54f5ab27d985c79743941036c9d3c
arnav-s1ngh <92050142+arnav-s1ngh@users.noreply.github.com>
2025-04-13 23:18:09 +0530

    Update client.py

--- Programming Exercise-4/client.py
+++ Programming Exercise-4/client.py
@@ -1,1 +1,45 @@
-
+# Client Side Application
+from time import sleep
+import requests
+import fitz
+import hashlib
+def hash_function(pth):
+    file=fitz.open(pth)
+    file_text=""
+    for pg in file:
+        file_text+=pg.get_text()
+    file.close()
+    print(f"File Content:- {file_text}")
+    file_hash=hashlib.sha256(file_text.encode('utf-8')).hexdigest()
+    print(f"File Hash:- {file_hash}")
+    return file_hash
+# hash_function("document.pdf")
+def add_timestamp_to_pdf(pth,timestamp,sig):
+    doc=fitz.open(pth)
+    f_page=doc[0]
+    f_page.insert_textbox(fitz.Rect(20, 20, 500, 100),f"Signature: {sig}\nTimestamp: {timestamp}",fontsize=1,color=(0.999,0.4,0.5))
+    new_file_loc=pth.replace(".pdf","_stamped.pdf")
+    doc.save(new_file_loc)
+    print(f"\nThe timestamped pdf was saved at:- {new_file_loc}\n")
+    doc.close()
+# add_timestamp_to_pdf("document.pdf","16:43","Joan Holloway Harris")
+def client_server_channel():
+    print("Client Program Initialising")
+    sleep(1)
+    print("\nNote:- This program is only capable of processing PDFs\n")
+    file_name=input("Enter the path for the file you wish to have time-stamped:- ")
+    file_hash=hash_function(file_name)
+    print(f"\nSending hash {file_hash} to timestamping server")
+    response=requests.post("http://localhost:5000/timestamp", json={"hash":file_hash})
+    if response.status_code==200:
+        print("\nRequest executed successfully\n")
+        result=response.json()
+        timestamp=result["timestamp"]
+        print(timestamp)
+        signature=result["signature"]
+        print(signature)
+        add_timestamp_to_pdf(file_name, timestamp, signature)
+    else:
+        print("Request failed gracefully")
+        print(response.status_code)
+client_server_channel()
