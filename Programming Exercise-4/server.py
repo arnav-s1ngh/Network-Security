27154adddff283949898c01c888c9e19ac860253
arnav-s1ngh <92050142+arnav-s1ngh@users.noreply.github.com>
2025-04-20 15:05:48 +0530

    Update server.py

--- Programming Exercise-4/server.py
+++ Programming Exercise-4/server.py
@@ -1,46 +1,47 @@
+# Server Side Application
+from flask import Flask,jsonify,request
 import rsa
-from flask import Flask, request, jsonify
 import ntplib
 import requests
 from datetime import datetime, timezone
-
-app=Flask(__name__)
+server_app=Flask(__name__)
 (public_key,private_key)=rsa.newkeys(2048)
-
 def get_timeapi_time():
+    resp=""
     try:
         response=requests.get("https://timeapi.io/api/Time/current/zone?timeZone=UTC")
         if response.status_code==200:
-            data = response.json()
-            return f"{data['date']} {data['time']}"
+            timeapi_time=response.json()
+            # print(timeapi_time)
+            return f"{timeapi_time['dateTime']}"
     except Exception as e:
-        print("Error fetching timeapi.io time:", e)
-    return "Unavailable"
-
+        print("Can't fetch timeapi.io time")
+        resp=e
+    return resp
+# get_timeapi_time()
 def get_ntp_time():
+    resp=""
     try:
-        client= ntplib.NTPClient()
-        response=client.request('time.nist.gov')
-        ntp_time=datetime.fromtimestamp(response.tx_time, tz=timezone.utc)
+        ntp_time=datetime.fromtimestamp(ntplib.NTPClient().request('time.nist.gov').tx_time,tz=timezone.utc)
+        # print(ntp_time)
         return ntp_time
     except Exception as e:
-        print("Error fetching NTP time:", e)
-    return "Unavailable"
+        print("Can't fetch NTP time")
+        resp=e
+    return resp
+# get_ntp_time()
 
-@app.route("/timestamp", methods=["POST"])
+@server_app.route("/timestamp",methods=["POST"])
 def timestamp():
     data=request.get_json()
-    doc_hash=data.get("hash")
-    if not doc_hash:
-        return jsonify({"error": "Missing 'hash' in request"}),400
-
+    pdf_hash=data.get("hash")
+    if pdf_hash is None:
+        return jsonify({"error":"Hash not received by Server"})
     timeapi_time=get_timeapi_time()
     ntp_time=get_ntp_time()
-    combined_time=f"timeapi: {timeapi_time}|ntp: {ntp_time}"
-
-    to_sign=f"{doc_hash}|{combined_time}".encode('utf-8')
-    signature=rsa.sign(to_sign, private_key, 'SHA-256')
-    response = {"timestamp": combined_time,"signature": signature.hex(),"public_key": public_key.save_pkcs1().decode('utf-8')}
+    combined_time=f"|| timeapi:{timeapi_time} || ntp:{ntp_time} ||"
+    to_sign=f"{pdf_hash}|{combined_time}".encode('utf-8')
+    signature=rsa.sign(to_sign,private_key, 'SHA-256')
+    response={"timestamp":combined_time,"signature":signature.hex(),"public_key":public_key.save_pkcs1().decode('utf-8')}
     return jsonify(response)
-
-app.run()
+server_app.run(ssl_context=('cert.pem','key.pem'))
